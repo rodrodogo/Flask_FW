9b16293b81c196d82af91390378cb0fe40d94fca
rodrodogo <rodrodogo@gmail.com>
2019-06-08 15:55:31 -0500

    interfaces

--- app.py
+++ app.py
@@ -8,31 +8,31 @@ app =Flask(__name__)
 
 @app.route('/')
 def index():
-	return app.send_static_file("index.html")
+	return render_template("index.html")
 @app.route('/crt_group')
 def crt_group():
-	return app.send_static_file("crt_group.html")
+	return render_template('crt_group.html')
 @app.route('/edt_group')
 def edt_group():
-	return app.send_static_file("edt_group.html")
+	return render_template("edt_group.html")
 @app.route('/rmv_group')
 def rmv_group():
-	return app.send_static_file("rmv_group.html")
+	return render_template("rmv_group.html")
 @app.route('/vw_group')
 def vw_group():
-	return app.send_static_file("vw_group.html")
+	return render_template("vw_group.html")
 @app.route('/crt_person')
 def crt_person():
-	return app.send_static_file("crt_person.html")
+	return render_template("crt_person.html")
 @app.route('/edt_person')
 def edt_person():
-	return app.send_static_file("edt_person.html")
+	return render_template("edt_person.html")
 @app.route('/rmv_person')
 def rmv_person():
-	return app.send_static_file("rmv_person.html")
+	return render_template("rmv_person.html")
 @app.route('/vw_person')
 def vw_person():
-	return app.send_static_file("vw_person.html")
+	return render_template("vw_person.html")
 
 
 @app.route('/insert', methods = ['POST'])
@@ -49,51 +49,23 @@ def insert():
 		newP = Person(identification = _id, name =_name, age = _age)
 		c.update(newP)
 
-	return app.send_static_file("success.html")
+	return render_template("index.html")
 
 @app.route('/vwPerson', methods = ['POST'])
 def vwPerson():
-	output = '''
-				<table style="width:100%">
-				  <tr>
-				    <th>identification</th>
-				    <th>name</th>
-				    <th>Age</th>
-				  </tr>
-			'''
+
 	_name = request.form['firstname']
 	s = DtoPerson();
 	_result = s.queryByName(_name)
 
-	output += ''' <tr><th>{0}</th>
-					<th>{1}</th>
-					<th>{2}</th>
-					</tr>
-				'''.format(_result.identification,_result.name,_result.age)
-
-	output += "</table>"
-	return output
+	return render_template("vwPerson.html", id = _result.identification, name = _result.name, age = _result.age)
 
 @app.route('/edtPerson', methods =['POST'])
 def edtPerson():
 	_name = request.form['firstname']
 	c = DtoPerson();
 	_result = c.queryByName(_name)
-	output = '''
-				<form action="/insert", method="post">
-				  id:<br>
-				  <input type="number" name="id"  value = "{0}"
-				  readonly="readonly">
-				  name:<br>
-				  <input type="text" name="firstnameM"  value = "{1}">
-				  <br>
-				  age:<br>
-				  <input type="number" name="age" value = "{2}">
-				  <br><br>
-				  <input type="submit" value="Submit">
-				</form>
-			'''.format(_result.identification,_result.name,_result.age)
-	return output
+	return render_template("edtPerson.html", id = _result.identification, name = _result.name, age = _result.age )
 
 @app.route('/rmvPerson', methods = ['POST'])
 def rmvPerson():
@@ -118,45 +90,21 @@ def insertGroup():
 		gruop = Groups(id = _id, name = _name)
 		c.update(gruop)
 
-	return app.send_static_file("success.html")
+	return render_template("index.html")
 
 @app.route('/vwGroup', methods = ['POST'])
 def vwGroup():
-	output = '''
-				<table style="width:100%">
-				  <tr>
-				    <th>id</th>
-				    <th>name</th>
-				  </tr>
-			'''
 	_name = request.form['name']
 	c = DtoGroups()
 	_result = c.queryByName(_name)
-	output += ''' <tr><th>{0}</th>
-					<th>{1}</th>
-					</tr>
-				'''.format(_result.id,_result.name)
-	output += "</table>"
-	return output
+	return render_template("vwGroup.html", id = _result.id, name = _result.name)
 
 @app.route('/edtGroup', methods =['POST'])
 def edtGroup():
 	_name = request.form['name']
 	c = DtoGroups()
 	_result = c.queryByName(_name)
-	output = '''
-				<form action="/insertGroup", method="post">
-				  id:<br>
-				  <input type="number" name="id"  value = "{0}"
-				  readonly="readonly">
-				  name:<br>
-				  <input type="text" name="nameM"  value = "{1}">
-				  <br>
-				  <br><br>
-				  <input type="submit" value="Submit">
-				</form>
-			'''.format(_result.id,_result.name)
-	return output
+	return render_template("edtGroup.html", id = _result.id, name = _result.name )
 
 @app.route('/rmvGroup', methods = ['POST'])
 def rmvGroup():
